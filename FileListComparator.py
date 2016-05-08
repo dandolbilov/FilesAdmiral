f53eb994f3f109b4c58f45e04cad886e51ce7c2e
Dan Dolbilov <d.dolbilov@gmail.com>
2016-05-08 16:54:55 +0300

    FileListComparator - implement "FileListComparator.py" and "test_FileListComparator.py"

---
+++ FileListComparator.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+    File:    FileListComparator.py
+    Author:  Daniil Dolbilov
+    Created: 08-May-2016
+"""
+
+import logging
+from pydal import DAL, Field
+
+
+class FileListComparator(object):
+    def __init__(self):
+        self.tempdb = None
+
+    def trace(self, event, msg):
+        logging.info('%s, %s' % (event, msg))
+
+    def prepareTempDatabase(self, tdbname):
+        self.trace('prepare-temp-db-start', tdbname)
+        if tdbname == 'sqlite:memory:':
+            self.tempdb = DAL(tdbname)
+            for tableName in ['table1', 'table2']:
+                self.tempdb.define_table(tableName,
+                                         Field('fname', type='string', length=256),
+                                         Field('fsize', type='integer'),
+                                         Field('md5_hash', type='string', length=32),
+                                         Field('sz_md5_name', type='string')
+                                         )
+        else:
+            self.trace('error', 'ERROR: tempdb <> memory is not implemented')
+        self.trace('prepare-temp-db-done', tdbname)
+        return self.tempdb
+
+    def prepareSnapshotTableFromImage2011(self, tableName, dbname):
+        self.trace('prepare-table-start', 'table=[%s], src=[%s]' % (tableName, dbname))
+
+        # connect to source database
+        db1 = DAL(dbname, migrate_enabled=False)
+        db1.define_table('Files',
+                         Field('fileId', type='integer'),
+                         Field('foId', type='integer'),
+                         Field('fname', type='text'),
+                         Field('fsize', type='integer'),
+                         Field('ctime', type='text'),
+                         Field('wtime', type='text'),
+                         primarykey=['fileId']
+                         )
+        db1.define_table('FilesMD5',
+                         Field('fileId', type='integer'),
+                         Field('md5', type='text'),
+                         Field('calcTime', type='text'),
+                         primarykey=['fileId']
+                         )
+        files_with_md5 = db1(db1.Files.fileId == db1.FilesMD5.fileId)
+
+        # clear dest temp table
+        self.tempdb(self.tempdb[tableName].id > 0).delete()
+
+        # copy data from source database to dest temp table
+        for x in files_with_md5(db1.Files.fileId > 0).select(limitby=(0, 150000)):  # TODO(dan.dolbilov): remove limit
+            self.tempdb[tableName].insert(
+                fname=x.Files.fname, fsize=x.Files.fsize, md5_hash=x.FilesMD5.md5,
+                sz_md5_name='%i_%s_%s' % (x.Files.fsize, x.FilesMD5.md5, x.Files.fname)
+            )
+        self.tempdb.commit()
+
+        # count rows in dest temp table
+        n = self.tempdb(self.tempdb[tableName].id > 0).count()
+        self.trace('prepare-table-done', 'table=[%s], count = %u' % (tableName, n))
+
+    def compare(self, dbname1, dbname2):
+        self.trace('compare-start', '')
+        self.prepareTempDatabase('sqlite:memory:')
+        self.prepareSnapshotTableFromImage2011('table1', dbname1)
+        self.prepareSnapshotTableFromImage2011('table2', dbname2)
+        tdb = self.tempdb
+
+        if 1:  # join v3 => GOOD (1 minute)
+            q2 = tdb()._select(tdb.table2.sz_md5_name)
+            for x in tdb(~tdb.table1.sz_md5_name.belongs(q2)).select():
+                self.trace('table1-unique', 'fname = [%s], fsize = %i, md5 = %s' % (x.fname, x.fsize, x.md5_hash))
+            self.trace('lastsql', tdb._lastsql)
+
+        self.trace('compare-done', '')
---
+++ test_FileListComparator.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+"""
+    File:    test_FileListComparator.py
+    Author:  Daniil Dolbilov
+    Created: 08-May-2016
+"""
+
+from __future__ import print_function
+
+import unittest
+import timeit
+from collections import Counter
+from FileListComparator import FileListComparator
+
+dbname1 = 'sqlite://D:/list-files/asus2data-2016.05.07.sqlite'
+dbname2 = 'sqlite://D:/list-files/asus2data-2016.05.08.sqlite'
+
+
+class TestFileListComparatorUsage(unittest.TestCase):
+
+    def test_compare_with_output(self):
+        from logging import DEBUG, INFO
+        from helpers import initLogs
+        initLogs(u"test_FileListComparator.log", fileAppend=False, fileLevel=DEBUG, consoleLevel=INFO)
+        self.comp = FileListComparator()
+        self.comp.compare(dbname1, dbname2)
+
+
+class TestFileListCompare(unittest.TestCase):
+
+    def fake_trace(self, event, msg):
+        self.events.append(event)
+
+    def setUp(self):
+        self.events = []
+        self.comp = FileListComparator()
+        self.comp.trace = self.fake_trace
+
+    @unittest.skip("temp off")
+    def test_compare(self):
+        self.comp.compare(dbname1, dbname2)
+        counter = Counter(self.events)
+        self.assertEqual(counter['compare-start'], 1)
+        self.assertEqual(counter['compare-done'], 1)
+        self.assertEqual(counter['prepare-temp-db-start'], 1)
+        self.assertEqual(counter['prepare-temp-db-done'], 1)
+        self.assertEqual(counter['prepare-table-start'], 2)
+        self.assertEqual(counter['prepare-table-done'], 2)
+        self.assertEqual(counter['table1-unique'], 6)
+
+
+class TestFileListJoinSpeed(unittest.TestCase):
+
+    def fake_trace(self, event, msg):
+        self.events.append(event)
+        if event == 'elapsed':
+            self.elapsed = msg
+
+    def setUp(self):
+        self.events = []
+        self.comp = FileListComparator()
+        self.comp.trace = self.fake_trace
+        self.comp.prepareTempDatabase('sqlite:memory:')
+        self.comp.prepareSnapshotTableFromImage2011('table1', dbname1)
+        self.comp.prepareSnapshotTableFromImage2011('table2', dbname2)
+
+    def time_usage(self, func):
+        def wrapper(*args, **kwargs):
+            start_time = timeit.default_timer()
+            func(*args, **kwargs)
+            elapsed = timeit.default_timer() - start_time
+            comp = args[0]
+            comp.trace('elapsed', '%.0f ms' % (elapsed * 1000))
+
+        return wrapper
+
+    @unittest.skip("temp off")
+    def test_sql_join_speed_v1(self):
+        @self.time_usage
+        def exec_sql_join_v1(comp):  # join v1 - SLOW (9 minutes)
+            tdb = self.comp.tempdb
+            for x in tdb(tdb.table2.fname == None).select(
+                    tdb.table1.ALL,
+                    tdb.table2.ALL,
+                    left=tdb.table2.on(tdb.table1.sz_md5_name == tdb.table2.sz_md5_name)):
+                comp.trace('table1-unique',
+                           'fname = [%s], fsize = %i, md5 = %s' % (x.table1.fname, x.table1.fsize, x.table1.md5_hash))
+
+        exec_sql_join_v1(self.comp)
+
+        counter = Counter(self.events)
+        self.assertEqual(counter['prepare-temp-db-start'], 1)
+        self.assertEqual(counter['prepare-temp-db-done'], 1)
+        self.assertEqual(counter['prepare-table-start'], 2)
+        self.assertEqual(counter['prepare-table-done'], 2)
+        self.assertEqual(counter['table1-unique'], 6)
+        self.assertEqual(counter['elapsed'], 1)
+
+    @unittest.skip("temp off")
+    def test_sql_join_speed_v2(self):
+        @self.time_usage
+        def exec_sql_join_v2(comp):  # join v2 - SLOW (9 minutes)
+            tdb = self.comp.tempdb
+            common_files = tdb(tdb.table1.sz_md5_name == tdb.table2.sz_md5_name)
+            common_list = [(lambda k: k['sz_md5_name'])(x) for x in
+                           common_files.select(tdb.table1.sz_md5_name, distinct=True).as_list()]
+            for x in tdb(tdb.table1.sz_md5_name).select():
+                if x.sz_md5_name not in common_list:
+                    comp.trace('table1-unique', 'fname = [%s], fsize = %i, md5 = %s' % (x.fname, x.fsize, x.md5_hash))
+
+        exec_sql_join_v2(self.comp)
+
+        counter = Counter(self.events)
+        self.assertEqual(counter['prepare-temp-db-start'], 1)
+        self.assertEqual(counter['prepare-temp-db-done'], 1)
+        self.assertEqual(counter['prepare-table-start'], 2)
+        self.assertEqual(counter['prepare-table-done'], 2)
+        self.assertEqual(counter['table1-unique'], 6)
+        self.assertEqual(counter['elapsed'], 1)
+
+    @unittest.skip("temp off")
+    def test_sql_join_speed_v3(self):
+        @self.time_usage
+        def exec_sql_join_v3(comp):  # join v3 - GOOD (1 minute)
+            tdb = self.comp.tempdb
+            q2 = tdb()._select(tdb.table2.sz_md5_name)
+            for x in tdb(~tdb.table1.sz_md5_name.belongs(q2)).select():
+                comp.trace('table1-unique', 'fname = [%s], fsize = %i, md5 = %s' % (x.fname, x.fsize, x.md5_hash))
+
+        exec_sql_join_v3(self.comp)
+
+        counter = Counter(self.events)
+        self.assertEqual(counter['prepare-temp-db-start'], 1)
+        self.assertEqual(counter['prepare-temp-db-done'], 1)
+        self.assertEqual(counter['prepare-table-start'], 2)
+        self.assertEqual(counter['prepare-table-done'], 2)
+        self.assertEqual(counter['table1-unique'], 6)
+        self.assertEqual(counter['elapsed'], 1)
+
+    def tearDown(self):
+        print('(elapsed = %s) ' % self.elapsed, end='')
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
