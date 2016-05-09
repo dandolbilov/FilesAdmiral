2d840687996416c4c00e7053e70d6f2a5a4f285f
Dan Dolbilov <d.dolbilov@gmail.com>
2016-05-09 23:53:51 +0300

    FilesExtractor - write MD5 file for extracted files ('unique-%date%.md5')

--- FilesExtractor.py
+++ FilesExtractor.py
@@ -7,6 +7,7 @@
 
 import os
 import shutil
+from datetime import datetime
 from FileListComparator import FileListComparator
 from helpers import normpathEx, calcFileMD5
 
@@ -20,6 +21,14 @@ class FilesExtractor(FileListComparator):
         self.trace('extract-files-start', '[%s] to [%s]' % (tableName, destPath))
         tdb = self.tempdb
 
+        if not os.path.exists(destPath):
+            os.makedirs(destPath)
+        md5_file = 'unique-%date%.md5'.replace('%date%', datetime.now().strftime("%Y.%m.%d"))
+        md5_file = normpathEx(destPath + '/') + md5_file
+        md5_head = '; FilesExtractor\n'
+        with open(md5_file, "wb") as f:
+            f.write(md5_head.encode('windows-1251'))  # TODO(dan.dolbilov): fix MD5 file encoding
+
         for x in tdb().select(tdb[tableName].ALL):
             a_dk_name = x.dk_name.decode('utf-8')
             a_fo_path = x.fo_path.decode('utf-8')
@@ -31,6 +40,10 @@ class FilesExtractor(FileListComparator):
                 os.makedirs(dir2)
             srcFile = dir1 + a_fname
             dstFile = dir2 + a_fname
+
+            md5_line = x.md5_hash + ' ' + normpathEx(a_dk_name + '/' + a_fo_path + '/') + a_fname + '\n'
+            with open(md5_file, "ab") as f:
+                f.write(md5_line.encode('windows-1251'))  # TODO(dan.dolbilov): fix MD5 file encoding
 
             if x.fsize > 100 * 1024 * 1024:
                 self.trace('error', 'ERROR: file copy skipped (fsize = %i), [%s] => [%s]' % (x.fsize, srcFile, dstFile))
